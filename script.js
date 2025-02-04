8eae2e5e80f081c7127cc3de617a34006306ca8d
luispaiva40200280 <luispaiva8936@gmail.com>
2025-02-04 23:01:22 +0000

    pokemon filterd by types almost got it

--- script.js
+++ script.js
@@ -2,41 +2,41 @@
 
 // Variables
 
-/**
- * @poKemon
- * 
- * 
- *
- */
-
 // DOM Elements
 const pokemonListUI = document.getElementById('pokemonList');
 const searchInput = document.querySelector('#pokemonName');
 const searchButton = document.querySelector('#searchButton');
 const form = document.querySelector('#searchForm');
+const sortBtn = document.getElementById('sort-btn');
+const pokemonsTypes = document.querySelector('#pokemonType');
 
 let pokemonList = [];
 // Fetch Pokémon Data
-export async function getPokemonData(){
+async function getPokemonData(){
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=200");
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150");
         const data = await response.json();
         pokemonList = data.results;
-        console.log(pokemonList);
-        console.log(data);
+        //console.log(pokemonList);
+        //console.log(data);
         getDetailPokemonData(pokemonList);
-        
+        sortBtn.addEventListener("click",()=>{
+            pokemonList.sort((a,b) => a.name.localeCompare(b.name));
+            pokemonListUI.innerHTML = '';
+            getDetailPokemonData(pokemonList);
+        })
+      
     } catch (error) {
         console.error(error);
         
     }
    
-}
+}//end
 
 // Display Pokémon Data
 function displayPokemonData(pokemonData){
     const {name , types , sprites} = pokemonData;
-    pokemonListUI.innerHTML +=  `<div class="card m-2" style="width: 18rem;">
+    return  `<div class="card m-2" style="width: 18rem;">
                             <img src="${sprites.front_default}" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">${name}</h5>
@@ -47,7 +47,9 @@ function displayPokemonData(pokemonData){
                                 <small class="text-muted">Last updated 3 mins ago</small>
                             </div>
                         </div>`
-}
+    //end
+    
+}//end
 function getTypes(types){
     let typesArray = [];
     types.forEach(type => {
@@ -56,18 +58,54 @@ function getTypes(types){
     return typesArray.join(', ');
 }
 function getDetailPokemonData(pokemonList){
+    let PokemonListDatail = []
     pokemonList.forEach(pokemon => {
         const pokemonData = pokemon.url;
         fetch(pokemonData).then(response => response.json()).then(data => {
-            displayPokemonData(data)
-           console.log(data);
+            PokemonListDatail.push(data)
+            pokemonListUI.innerHTML += displayPokemonData(data)
+             
         });
     });
-}
+   return PokemonListDatail
+}//end of the funcion
+// Search functionality by types
 
-getPokemonData();
+
+pokemonsTypes.addEventListener('change', (e) => {
+    e.preventDefault();
+    const selectedType = e.target.value;
+    if (selectedType === ''){
+        pokemonListUI.innerHTML = '';
+        getDetailPokemonData(pokemonList);
+        return;
+    }
+    fetch(`https://pokeapi.co/api/v2/type/${selectedType}`).then(response => response.json()).then(data => {
+       const poketType = data.pokemon
+       poketType.forEach(pokemon =>{
+        const pokemonData = pokemon.pokemon.url
+        fetch(pokemonData).then(response => response.json()).then(data => {
+            pokemonListUI.innerHTML = ''
+            pokemonListUI.innerHTML += displayPokemonData(data)
+             
+        });
+    
+       })
+        console.log(data)
+        console.log(poketType)
+         
+    });
+    console.log(e.target.value);
+    
+}) 
+
 // Event Listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('Form Submitted');
 })
+
+
+
+
+getPokemonData();
